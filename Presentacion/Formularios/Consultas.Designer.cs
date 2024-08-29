e656c240336c907c6d996f324e1bae1dbdeab0c4
linker <diegolucas142@gmail.com>
2024-08-28 21:48:38 -0300

    diseño A

--- Presentacion/Formularios/Consultas.Designer.cs
+++ Presentacion/Formularios/Consultas.Designer.cs
@@ -28,12 +28,362 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Consultas));
+            this.pFormularios = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pBarraSuperior = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pContenedor = new System.Windows.Forms.Panel();
+            this.pFecha = new System.Windows.Forms.Panel();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnConsultas = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.pLogo = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pFormularios.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.pBarraSuperior.SuspendLayout();
+            this.pContenedor.SuspendLayout();
+            this.pFecha.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pFormularios
+            // 
+            this.pFormularios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(167)))), ((int)(((byte)(173)))));
+            this.pFormularios.Controls.Add(this.label6);
+            this.pFormularios.Controls.Add(this.label5);
+            this.pFormularios.Controls.Add(this.dataGridView1);
+            this.pFormularios.Controls.Add(this.label4);
+            this.pFormularios.Controls.Add(this.panel1);
+            this.pFormularios.Location = new System.Drawing.Point(180, 32);
+            this.pFormularios.Name = "pFormularios";
+            this.pFormularios.Size = new System.Drawing.Size(770, 468);
+            this.pFormularios.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(303, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Registrar Tareas";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(48, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(694, 221);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(30, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tarea:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(447, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fecha y Hora:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(30, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 126);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(642, 23);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(447, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(291, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pBarraSuperior
+            // 
+            this.pBarraSuperior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(34)))), ((int)(((byte)(50)))));
+            this.pBarraSuperior.Controls.Add(this.panel4);
+            this.pBarraSuperior.Controls.Add(this.panel3);
+            this.pBarraSuperior.Controls.Add(this.panel2);
+            this.pBarraSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pBarraSuperior.Location = new System.Drawing.Point(0, 0);
+            this.pBarraSuperior.Name = "pBarraSuperior";
+            this.pBarraSuperior.Size = new System.Drawing.Size(950, 32);
+            this.pBarraSuperior.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(800, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(50, 32);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(850, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(50, 32);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(900, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(50, 32);
+            this.panel2.TabIndex = 0;
+            // 
+            // pContenedor
+            // 
+            this.pContenedor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(34)))), ((int)(((byte)(50)))));
+            this.pContenedor.Controls.Add(this.pFecha);
+            this.pContenedor.Controls.Add(this.btnAdmin);
+            this.pContenedor.Controls.Add(this.btnConsultas);
+            this.pContenedor.Controls.Add(this.btnInicio);
+            this.pContenedor.Controls.Add(this.pLogo);
+            this.pContenedor.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pContenedor.Location = new System.Drawing.Point(0, 32);
+            this.pContenedor.Name = "pContenedor";
+            this.pContenedor.Size = new System.Drawing.Size(180, 468);
+            this.pContenedor.TabIndex = 5;
+            // 
+            // pFecha
+            // 
+            this.pFecha.Controls.Add(this.lblHora);
+            this.pFecha.Controls.Add(this.lblFecha);
+            this.pFecha.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pFecha.Location = new System.Drawing.Point(0, 379);
+            this.pFecha.Name = "pFecha";
+            this.pFecha.Size = new System.Drawing.Size(180, 89);
+            this.pFecha.TabIndex = 4;
+            // 
+            // lblHora
+            // 
+            this.lblHora.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblHora.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblHora.ForeColor = System.Drawing.Color.White;
+            this.lblHora.Location = new System.Drawing.Point(0, 45);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(180, 44);
+            this.lblHora.TabIndex = 1;
+            this.lblHora.Text = "HORA";
+            this.lblHora.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblFecha.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFecha.ForeColor = System.Drawing.Color.White;
+            this.lblFecha.Location = new System.Drawing.Point(0, 0);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(180, 45);
+            this.lblFecha.TabIndex = 0;
+            this.lblFecha.Text = "FECHA";
+            this.lblFecha.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(24)))), ((int)(((byte)(35)))));
+            this.btnAdmin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdmin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdmin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdmin.ForeColor = System.Drawing.Color.White;
+            this.btnAdmin.Image = ((System.Drawing.Image)(resources.GetObject("btnAdmin.Image")));
+            this.btnAdmin.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAdmin.Location = new System.Drawing.Point(0, 300);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(180, 75);
+            this.btnAdmin.TabIndex = 3;
+            this.btnAdmin.Text = "Administracion";
+            this.btnAdmin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAdmin.UseVisualStyleBackColor = false;
+            // 
+            // btnConsultas
+            // 
+            this.btnConsultas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(24)))), ((int)(((byte)(35)))));
+            this.btnConsultas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConsultas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConsultas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConsultas.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnConsultas.ForeColor = System.Drawing.Color.White;
+            this.btnConsultas.Image = ((System.Drawing.Image)(resources.GetObject("btnConsultas.Image")));
+            this.btnConsultas.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnConsultas.Location = new System.Drawing.Point(0, 225);
+            this.btnConsultas.Name = "btnConsultas";
+            this.btnConsultas.Size = new System.Drawing.Size(180, 75);
+            this.btnConsultas.TabIndex = 2;
+            this.btnConsultas.Text = "Consultas";
+            this.btnConsultas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnConsultas.UseVisualStyleBackColor = false;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(24)))), ((int)(((byte)(35)))));
+            this.btnInicio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInicio.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnInicio.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnInicio.ForeColor = System.Drawing.Color.White;
+            this.btnInicio.Image = ((System.Drawing.Image)(resources.GetObject("btnInicio.Image")));
+            this.btnInicio.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnInicio.Location = new System.Drawing.Point(0, 150);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnInicio.Size = new System.Drawing.Size(180, 75);
+            this.btnInicio.TabIndex = 1;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnInicio.UseVisualStyleBackColor = false;
+            // 
+            // pLogo
+            // 
+            this.pLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pLogo.Location = new System.Drawing.Point(0, 0);
+            this.pLogo.Name = "pLogo";
+            this.pLogo.Size = new System.Drawing.Size(180, 150);
+            this.pLogo.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 315);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(694, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(303, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Visualiza el historial de tareas";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(292, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(194, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Historico de Tareas";
+            // 
+            // Consultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(950, 500);
+            this.Controls.Add(this.pContenedor);
+            this.Controls.Add(this.pBarraSuperior);
+            this.Controls.Add(this.pFormularios);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Consultas";
             this.Text = "Consultas";
+            this.pFormularios.ResumeLayout(false);
+            this.pFormularios.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.pBarraSuperior.ResumeLayout(false);
+            this.pContenedor.ResumeLayout(false);
+            this.pFecha.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel pFormularios;
+        private Panel pBarraSuperior;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel2;
+        private Panel pContenedor;
+        private Panel pFecha;
+        private Label lblHora;
+        private Label lblFecha;
+        private Button btnAdmin;
+        private Button btnConsultas;
+        private Button btnInicio;
+        private Panel pLogo;
+        private Panel panel1;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label6;
+        private Label label5;
+        private DataGridView dataGridView1;
     }
 }
