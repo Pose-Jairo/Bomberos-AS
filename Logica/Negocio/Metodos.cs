86df7331954c5348baf94780a3e3cf924fc3b24d
Jairo Pose <megajairopose@hotmail.com>
2024-10-15 19:19:10 -0300

    Codigo Consultas y Arreglo de ConexionConBD

---
+++ Logica/Negocio/Metodos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using Datos.Conexion;
+
+namespace Logica.Negocio
+{
+    public class Metodos
+    {
+        ConexionConBD BDConexion = new ConexionConBD();
+
+        public DataTable Actualizar(DataTable Tabla, string consulta)
+        {
+            Tabla = new DataTable();
+
+            Tabla.Clear();
+
+            Tabla.Load(BDConexion.LectorSql(consulta));
+
+            BDConexion.Desconectar();
+
+            return (Tabla);
+        }
+    }
+}
---
+++ Logica/Negocio/Validaciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.Conexion;
+
+namespace Logica.Negocio
+{
+    public class Validaciones
+    {
+        ConexionConBD BDConexion = new ConexionConBD();
+
+        public void PruebaAbm(string consulta)
+        {
+            bool ABM = false;
+
+            ABM = BDConexion.EjecutarConsulta(consulta);
+
+            if (ABM == true)
+            {
+
+            }
+            else
+            {
+               
+            }
+        }
+    }
+}
