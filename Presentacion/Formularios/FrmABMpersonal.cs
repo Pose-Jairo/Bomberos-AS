e53bad6f154442eb0ed753a87454623590e2d823
Lucas716253 <lucasibarralhxd@gmail.com>
2024-10-21 21:19:46 -0300

    actualizacion_personal
    dada

--- Presentacion/Formularios/FrmABMpersonal.cs
+++ Presentacion/Formularios/FrmABMpersonal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Datos.Conexion;
+using Logica.Negocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,5 +24,49 @@ namespace Presentacion.Formularios
             var button = ((Button)sender);
             PrincipalFrm.Instancia.AbrirFormulario(button.Name);
         }
+
+        ConexionConBD ClaseConexion = new ConexionConBD();
+        Metodos metodo = new Metodos();
+        DataTable Tabla = new DataTable();
+
+
+
+        private void FrmABMpersonal_Load(object sender, EventArgs e)
+        {
+            Tabla.Clear();
+            Tabla.Load(ClaseConexion.LectorSql("Select * from bombero"));
+            //asigna los datos a la tabla...
+            dgvTablaPersonal.DataSource = Tabla;
+
+            ClaseConexion.Desconectar();
+
+            //llama a los metodos que llena los combobox...
+            cmbRango.DisplayMember = "rango";
+            cmbRango.ValueMember = "cod_rango";
+            cmbRango.DataSource = metodo.Carga_Rango();
+
+            cmbFiltraRango.DisplayMember = "rango";
+            cmbFiltraRango.ValueMember = "cod_rango";
+            cmbFiltraRango.DataSource = metodo.Carga_Rango();
+
+            //llama a los metodos que llena los combobox...
+            cmbArea.DisplayMember = "nombre";
+            cmbArea.ValueMember = "dod_area";
+            cmbArea.DataSource = metodo.Carga_Area();
+
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            //ejecuta el metodo de insert...
+            //variable de control...
+            bool alta = false;
+            int area = cmbArea.SelectedIndex + 1;
+            int rango = cmbRango.SelectedIndex + 1;
+            //consulta de insert...
+            alta = ClaseConexion.EjecutarConsulta("Insert into bombero(nombre, apellido, cod_rango, cod_area) VALUES('" + txtNombre.Text + "','" + txtApellido.Text + "'," + rango + "," + area + ")");
+        }
+
+
     }
 }
